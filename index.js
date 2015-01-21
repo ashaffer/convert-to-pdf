92ce339590d95b95daac652d34b48a3addc38587
Andrew Shaffer <andrew@weo.io>
2015-01-20 19:25:30 -0800

    fix

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var unoconv = require('unoconv');
 var path = require('path');
 var fs = require('fs');
 
-module.exports = function(name, cb) {
-  var ext = path.extname(name);
+module.exports = function(fileName, opts, cb) {
+  var type;
+  opts = opts || {};
+
+  type = path.extname(fileName).slice(1);
 
   // If it's already a pdf, just return the file as a Buffer
-  if(ext === '.pdf') {
-    cb(null, fs.readFileSync(name));
+  if(type === 'pdf') {
+    cb(null, fs.readFileSync(fileName));
     return;
   }
 
-  unoconv.convert(name, 'pdf', cb);
+  unoconv.convert(fileName, 'pdf', cb);
 };
