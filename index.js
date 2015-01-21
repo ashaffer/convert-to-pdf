d3dd4ae3f362418fa1ed11e4665bb1326635beac
Andrew Shaffer <andrew@weo.io>
2015-01-20 17:15:29 -0800

    add streams

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var unoconv = require('unoconv');
+var stream = require('stream');
 var path = require('path');
 
 module.exports = function(name, cb) {
@@ -6,9 +7,19 @@ module.exports = function(name, cb) {
 
   // If it's already a pdf, just return the file as a Buffer
   if(ext === '.pdf') {
-    cb(null, fs.readFileSync(name));
+    cb(null, fs.readReadStream(name));
     return;
   }
 
-  unoconv.convert(name, 'pdf', cb);
+  var bufStream = new stream.PassThrough();
+  unoconv.convert(name, 'pdf', function(err, buf) {
+    if(err) {
+      bufStream.emit('error', err);
+      return;
+    }
+
+    bufStream.end(buf);
+  });
+
+  return bufStream;
 };
